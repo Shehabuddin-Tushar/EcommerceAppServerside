189743b29cbc2279edd585fd475842f656beb0c6
Tushar <freelancerrtushar@gmail.com>
2022-01-04 13:31:51 +0600

    commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const SSLCommerzPayment = require('sslcommerz') 
-const { MongoClient } = require('mongodb');
+ const { MongoClient } = require('mongodb');
 const ObjectId = require("mongodb").ObjectId;
 var cors = require('cors')
 require('dotenv').config()
+const stripe = require("stripe")(process.env.PAYMENT_STRIPE_SECRETE_KEY)
 const port = process.env.PORT || 5000
 const multer = require('multer')
 const upload = multer({ dest: 'uploads/' })
@@ -19,82 +19,24 @@ async function run() {
         await client.connect();
         const database = client.db('Ecommerce');
         const products = database.collection('products');
-       
+        const addtocart = database.collection('addtocart');
 
         app.get("/products", async (req, res) => {
                const result=await products.find({}).toArray();
                res.send(result)
         })
 
+
+        
+
+        app.post("/addtocart", async (req, res) => {
+            const result = await addtocart.insertOne(req.body);
+            res.send(result);
+        });
+
        
-
-        //payment initialization api
-        app.get('/init', (req, res) => {
-            const data = {
-                
-                total_amount: 100,
-                currency: 'BDT',
-                tran_id: 'REF123',
-                success_url: `${process.env.PORT}/success`,
-                fail_url: `${process.env.PORT}/fail`,
-                cancel_url: `${process.env.PORT}/cancel`,
-                ipn_url: `${process.env.PORT}/ipn`,
-                shipping_method: 'Courier',
-                product_name: 'Computer.',
-                product_category: 'Electronic',
-                product_profile: 'general',
-                cus_name: 'Customer Name',
-                cus_email: 'cust@yahoo.com',
-                cus_add1: 'Dhaka',
-                cus_add2: 'Dhaka',
-                cus_city: 'Dhaka',
-                cus_state: 'Dhaka',
-                cus_postcode: '1000',
-                cus_country: 'Bangladesh',
-                cus_phone: '01711111111',
-                cus_fax: '01711111111',
-                ship_name: 'Customer Name',
-                ship_add1: 'Dhaka',
-                ship_add2: 'Dhaka',
-                ship_city: 'Dhaka',
-                ship_state: 'Dhaka',
-                ship_postcode: 1000,
-                ship_country: 'Bangladesh',
-                multi_card_name: 'mastercard',
-                value_a: 'ref001_A',
-                value_b: 'ref002_B',
-                value_c: 'ref003_C',
-                value_d: 'ref004_D'
-            };
-            const sslcommer = new SSLCommerzPayment(process.env.STORE_ID,process.env.STORE_PASSWORD,false) //true for live default false for sandbox
-            sslcommer.init(data).then(data => {
-               
-                //process the response that got from sslcommerz
-                //https://developer.sslcommerz.com/doc/v4/#returned-parameters
-                res.redirect(data.GatewayPageURL);
-            });
-        })
-
-        app.post("/success", async (req, res) => {
-            console.log(req.body)
-            res.status(200).json({data:req.body})
-        })
-
-        app.post("/fail", async (req, res) => {
-            console.log(req.body)
-            res.status(400).json({ data: req.body })
-        })
-
-        app.post("/cancel", async (req, res) => {
-            console.log(req.body)
-            res.status(200).json({ data: req.body })
-        })
-
-        app.post("/ipn", async (req, res) => {
-            console.log(req.body)
-            res.status(200).json({ data: req.body })
-        })
-
+      
+       
 
         
     } finally {
